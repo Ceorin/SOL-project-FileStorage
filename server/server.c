017d022ee85fb25b3fe6b7f69d93424ffdf7eba1
Enri <enricoguerraaet@gmail.com>
2022-06-07 18:30:48 +0200

    basic testing WIP
    I need to move to a different machine for a while

--- server/server.c
+++ server/server.c
@@ -7,36 +7,59 @@
 #define CONFIG_PATH "../config/config.txt"
 #define true 1
 #define false 0
-#define test_perror_NULL(sc, msg) \
+#define test_null (sc, msg) \
     if ((sc) == NULL ) { perror (msg); exit(EXIT_FAILURE); }
-#define test_perror_NEG1(sc, msg) \
-    if ((sc) == -1 ) { perror (msg); exit(EXIT_FAILURE); }
+#define test_error (comp, sc, msg) \
+    if ((sc) == (comp) ) { perror (msg); exit(EXIT_FAILURE); }
+#define test_error_isNot (comp, sc, msg) \
+    if ((sc) != (comp) ) { perror (msg); exit(EXIT_FAILURE); }
 
-struct config {
+typedef struct config {
     unsigned int file_num;
     unsigned int cache_size;
     unsigned short thread_num;
     char *server_socket_name; 
     char *log_file_name;
-} config;
+} _config;
 
-int readConfig (char* path) { // returns 0 if okay, other codes for errors
-    int i = 5;
+void readConfig (char* path) { 
+    int i = 0;
+    char buffer [64];
+    char *key, *strVal;
+    unsigned int intVal;
+    unsigned short shortVal;
     FILE * inp;
-    test_perror_NULL(inp = fopen (path, "r"), "Opening config file: ");
+    test_null ((inp = fopen (path, "r")), "Opening config file");
+    fprintf(stdout, "Opening config file: Success!\n");
+    while (i<5 || !feof (inp)) {
+    	test_null (fgets(buffer, 64, inp), "Reading config file");
+    	switch (i) {
+    	  case 0:
+    	  	test_error_isNot (2, sscanf(buffer, "%s %d", key, intVal);
+    	  case 1:
+    	  	test_error_isNot (2, sscanf(buffer, "%s %d", key, intVal);
+    	  case 2:
+    	  	test_error_isNot (2, sscanf(buffer, "%s %d", key, shortVal);
+    	  case 3:
+    	  	test_error_isNot (2, sscanf(buffer, "%s %d", key, strVal);
+    	  case 4:
+    	  	test_error_isNot (2, sscanf(buffer, "%s %d", key, strVal);
+    	}
+    	fprtinf(stdout, "TEST of Read: %s\n", &key);
+    	i++;
+    }
 
+    test_error_isNot(0, fclose(inp), "Closing config file");
+    fprintf(stdout, "Read config file: Success!\nLeaving readConfig()\n");
 }
 
 int main (int argc, char *argv[]) {
-    if (argc > 2) { 
-        fprintf(stderr, "Starting server - the only possible parameter for the application is the config file");
-        exit(EXIT_FAILURE);
+    if (argc > 2) { fprintf(stderr, "Starting server: the only possible parameter for the application is the config file\n"); exit(EXIT_FAILURE);
     }
-    int e;
-    if (argc == 2) 
-        e = readConfig (argv[1]);
-    else
-        e = readConfig ("../config/config.txt");
-    if (e!=0) return 2;
-    return 0;
+    
+    // readd Config file
+    if (argc == 2) readConfig (argv[1]);
+    else readConfig ("../config/config.txt");
+    
+    exit(EXIT_SUCCESS);
 }
